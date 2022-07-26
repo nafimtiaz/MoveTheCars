18678e0385728c6a7a053d4fe937594d048e004a
Nafiz Imtiaz <nafiz2009@gmail.com>
2022-07-26 22:28:56 +0600

    Added the main home view class and prefab

---
+++ Assets/Scripts/MTC/Views/MTCHomeView.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MTC.Utils;
+using UnityEngine;
+
+public class MTCHomeView : MonoBehaviour
+{
+    public LevelData LevelData { get; set; }
+    private LevelData currentLevelData;
+
+
+    private void Start()
+    {
+        GenerateScene();
+    }
+    
+    void GenerateScene()
+    {
+        PlaceParkingLotObjects(0);
+    }
+
+    void PlaceParkingLotObjects(int levelIndex)
+    {
+        GameData gameData = DataManager.LoadDataFromJson<GameData>();
+        currentLevelData = gameData.levelData[levelIndex];
+
+        foreach (var objData in currentLevelData.lotObjects)
+        {
+            ObjectCreator.CreateAndPlaceObject(objData);
+        }
+    }
+}
